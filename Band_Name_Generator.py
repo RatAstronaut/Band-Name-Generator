c5e14ba07bc1e97e3366ea237b10b7f1bdc3c207
RatAstronaut <rufushouse@yahoo.com>
2024-11-15 13:29:20 -0600

    Add files via upload
    This is a simple Band Name Generator I made in Pycharm

---
+++ Band_Name_Generator.py
@@ -0,0 +1,7 @@
+# This is a Band Name Generator. key things to note is you can condense alot of lines by going right off the bat
+# into assigning variables. Because when the code is run in the terminal the output doesnt show the variable at all.
+# To get the cursor/user input on a new line utilize \n which will automatically send the code to the next line.
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be: " + city + " " + pet)
